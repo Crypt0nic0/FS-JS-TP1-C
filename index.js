576bb39b0c7b4cb86146bbfb8b0a458a517b9988
Crypt0nic0 <n.greneche45@gmail.com>
2023-03-21 12:01:41 +0100

    TP Finished

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-// Créer 3 ronds de tailles différentes (dont un qui remplacera la souris)
 
-// Ajouter un événement sur la fenetre (window) puis animer la position de ces ronds (top, left injecter "e")
+const mouses = document.querySelectorAll(".mouse");
 
-// S'assurer que les liens sont clickables
+window.addEventListener('mousemove', (e) =>{
+    mouses.forEach((mouse) => {
+        mouse.style.top = e.y + "px";
+        mouse.style.left = e.x + "px";
+    });
+});
 
-// Donner un style de transparence aux 2 plus gros ronds (mix-blend-mode)
+/*
+window.addEventListener('mousemove', (e) =>{
+    cursor.style.top = e.y + "px";
+    cursor.style.left = e.x + "px";
+
+    mouse1.style.top = e.y + "px";
+    mouse1.style.left = e.x + "px";
+
+    cursor.style.top = e.y + "px";
+    cursor.style.left = e.x + "px";
+}*/
